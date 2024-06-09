7dd469053225841e7beca519805c76988e13ff10
Igor Kucia <redvers@protonmail.com>
2024-06-09 20:03:18 +0200

    npc

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleGame
 {
@@ -12,14 +13,25 @@ namespace ConsoleGame
         static int playerX = 20;
         static int playerY = 10;
 
-        // tablica
+        // pozycja NPC
+        static int npcX = 5;
+        static int npcY = 5;
+
+        // pozycja przedmiotu
+        static int itemX = 15;
+        static int itemY = 15;
+
+        // czy gracz podnisol przedmiot
+        static bool hasItem = false;
+
+
         static bool[,] obstacles = new bool[width, height];
 
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
 
-            // ręczne dodanie przeszkod
+            // ręczne dodanie przeszkód
             AddObstacles();
 
             while (true)
@@ -29,12 +41,20 @@ namespace ConsoleGame
 
                 // input ruchu
                 HandleInput();
+
+                // ruch NPC
+                MoveNPC();
+
+                // sprawdź czy NPC jest blisko gracza
+                CheckNPCProximity();
+
+                // sprawdź czy gracz podniósł przedmiot
+                CheckItemPickup();
             }
         }
 
         static void AddObstacles()
         {
-            // reczne dodanie przeszkód
 
             obstacles[10, 5] = true;
             obstacles[15, 8] = true;
@@ -55,6 +75,14 @@ namespace ConsoleGame
                     {
                         Console.Write('X'); // gracz
                     }
+                    else if (x == npcX && y == npcY)
+                    {
+                        Console.Write('@'); // NPC
+                    }
+                    else if (x == itemX && y == itemY && !hasItem)
+                    {
+                        Console.Write('*'); // przedmiot
+                    }
                     else if (obstacles[x, y])
                     {
                         Console.Write('|'); // przeszkoda
@@ -65,6 +93,14 @@ namespace ConsoleGame
                     }
                 }
                 Console.WriteLine();
+            }
+
+            // wyświetl informacje o inwentarzu
+            Console.SetCursorPosition(0, height);
+            Console.Write("Inwentarz: ");
+            if (hasItem)
+            {
+                Console.Write("Przedmiot");
             }
         }
 
@@ -87,6 +123,65 @@ namespace ConsoleGame
                 case ConsoleKey.RightArrow:
                     if (playerX < width - 1 && !obstacles[playerX + 1, playerY]) playerX++;
                     break;
+            }
+        }
+
+        static void MoveNPC()
+        {
+            // npc ruch
+            Random rnd = new Random();
+            int direction = rnd.Next(0, 4);
+
+            int newNPCX = npcX;
+            int newNPCY = npcY;
+
+            switch (direction)
+            {
+                case 0: // gora
+                    if (npcY > 0 && !obstacles[npcX, npcY - 1]) newNPCY--;
+                    break;
+                case 1: // dol
+                    if (npcY < height - 1 && !obstacles[npcX, npcY + 1]) newNPCY++;
+                    break;
+                case 2: // lewo
+                    if (npcX > 0 && !obstacles[npcX - 1, npcY]) newNPCX--;
+                    break;
+                case 3: // prawo
+                    if (npcX < width - 1 && !obstacles[npcX + 1, npcY]) newNPCX++;
+                    break;
+            }
+
+            // aktualizacja pozycji nmpc
+            npcX = newNPCX;
+            npcY = newNPCY;
+        }
+
+        static void CheckNPCProximity()
+        {
+            // sprawdzanie czy NPC jest blisko
+            if (Math.Abs(playerX - npcX) <= 1 && Math.Abs(playerY - npcY) <= 1)
+            {
+                // npc jest blisko
+                Console.SetCursorPosition(Console.WindowWidth / 2 - 3, Console.WindowHeight / 2);
+                Console.Write("NPC: Witaj!");
+
+                // delay textu
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        static void CheckItemPickup()
+        {
+            // sprawdzanie podnoszenia itemu
+            if (playerX == itemX && playerY == itemY && !hasItem)
+            {
+                hasItem = true;
+                // usuń przedmiot z planszy
+                itemX = -1;
+                itemY = -1;
             }
         }
     }
